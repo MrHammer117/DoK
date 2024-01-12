690615ab64c90a57dfede1d3066ee6d717244f1f
MrHammer117 <Starkey2893@gmail.com>
2024-01-12 12:19:03 -0500

    Implement Moving Character

--- Player/playerrun.gd
+++ Player/playerrun.gd
@@ -1,28 +1,25 @@
 extends CharacterBody2D
+const MAX_SPEED = 150
+const FRICTION = 500
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+const ACCELERATION = 500
+var movement = Vector2.ZERO
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var input_vector = Vector2.ZERO
+	#This line below takes input from the user and transmits it straight to the character
+	#left and right
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	#up and down
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized( )
+	if input_vector != Vector2.ZERO:
+		movement = movement.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		print(movement)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		movement = movement.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+		
+	# The velocity (movement) is multiplied by delta (the framerate) to keep the game consistent
+	# This is pixels per second now instead of pixels per frame
+	move_and_collide(movement * delta)
