a1cea8c60d327dc192abb9ee3ad7835747d46e56
MrHammer117 <Starkey2893@gmail.com>
2024-01-12 15:26:58 -0500

    Implement Collision Test

--- Player/playerrun.gd
+++ Player/playerrun.gd
@@ -3,8 +3,8 @@ const MAX_SPEED = 150
 const FRICTION = 500
 
 const ACCELERATION = 500
-var movement = Vector2.ZERO
-
+# I do not need to create velocity or what I originally had movement, because velocity is alreayd a 
+# member of the class CharacterBody2D
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	#This line below takes input from the user and transmits it straight to the character
@@ -14,12 +14,12 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized( )
 	if input_vector != Vector2.ZERO:
-		movement = movement.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-		print(movement)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		print(velocity)
 	else:
-		movement = movement.move_toward(Vector2.ZERO, FRICTION * delta)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 		
-	# The velocity (movement) is multiplied by delta (the framerate) to keep the game consistent
-	# This is pixels per second now instead of pixels per frame
-	move_and_collide(movement * delta)
+	#This automatically uses the variable velocity, unlike the tutorial I am watching I do not need
+	#to pass a variable, this already does everything for me!!!
+	move_and_slide()
