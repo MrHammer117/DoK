96de60557cc1a50392b7896a26b1d889558b1d75
MrHammer117 <Starkey2893@gmail.com>
2024-11-07 13:36:14 -0500

    clean player movement code

--- Player/playerrun.gd
+++ Player/playerrun.gd
@@ -1,28 +1,22 @@
 extends CharacterBody2D
-const MAX_SPEED = 150
-const FRICTION = 500
 
 var enemy_inattack_range = false
 var enemy_attack_cooldown = true
 var health = 100
 var player_alive = true
+ 
+var bulletSpeed = 3000
+var bullet = preload("res://Weapons/Bullet.tscn")
 
 
-@export var speed = 400
+@export var speed = 250
 
 func _ready():
 	add_to_group("Player")
-# I do not need to create velocity or what I originally had movement, because velocity is alreayd a 
-# member of the class CharacterBody2D
 
 func get_input(delta):
-	
-	var motion = Vector2()
-	
 	position += Input.get_vector("left", "right", "up", "down") * speed * delta
-	
 	look_at(get_global_mouse_position())
-	
 
 func _physics_process(delta):
 	get_input(delta)
@@ -31,6 +25,9 @@ func _physics_process(delta):
 func player():
 	pass
 
+func fire():
+	var bullet_instance
+	#finish implementation
 
 func _on_hitbox_body_entered(body):
 	if body.has_method("enemy"):
