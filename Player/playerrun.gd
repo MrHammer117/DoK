174786d07424a614c10352e273e1ec1f2859fbfe
MrHammer117 <Starkey2893@gmail.com>
2024-11-14 00:06:56 -0500

    add projectiles

--- Player/playerrun.gd
+++ Player/playerrun.gd
@@ -5,29 +5,40 @@ var enemy_attack_cooldown = true
 var health = 100
 var player_alive = true
  
-var bulletSpeed = 3000
+var bulletSpeed = 300
 var bullet = preload("res://Weapons/Bullet.tscn")
 
 
-@export var speed = 250
+const MOVE_SPEED: float = 500
+@export var speed_multiplier: int = 1
 
 func _ready():
 	add_to_group("Player")
 
-func get_input(delta):
-	position += Input.get_vector("left", "right", "up", "down") * speed * delta
-	look_at(get_global_mouse_position())
-
-func _physics_process(delta):
-	get_input(delta)
+func move() -> void:
+	var movement: Vector2 = Vector2.ZERO
+	if Input.is_action_pressed("left"): movement.x -= 1.0
+	if Input.is_action_pressed("right"): movement.x += 1.0
+	if Input.is_action_pressed("up"): movement.y -= 1.0
+	if Input.is_action_pressed("down"): movement.y += 1.0
+	velocity = movement.normalized() * (MOVE_SPEED * speed_multiplier)
 	move_and_slide()
+	
+
+func _physics_process(delta: float) -> void:
+	move()
+	look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("click"):
+		fire()
 
 func player():
 	pass
 
 func fire():
-	var bullet_instance
-	#finish implementation
+	var bullet_instance = bullet.instantiate()
+	owner.add_child(bullet_instance)
+	bullet_instance.transform = $ShootFromHere.global_transform
+	
 
 func _on_hitbox_body_entered(body):
 	if body.has_method("enemy"):
