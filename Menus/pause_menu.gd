322dc59d4cac348ca9c9aa897afc442ee7589836
MrHammer117 <Starkey2893@gmail.com>
2024-11-20 21:53:50 -0500

    pause menu functionality

---
+++ Menus/pause_menu.gd
@@ -0,0 +1,29 @@
+extends Control
+
+var _is_paused:bool = false:
+	set = set_paused
+
+func _unhandled_input(event:InputEvent) -> void:
+	if event.is_action_pressed("escape"):
+		_is_paused = !_is_paused
+
+
+func set_paused(value:bool) -> void:
+	_is_paused = value
+	get_tree().paused = _is_paused
+	visible = _is_paused
+	
+
+func _on_resume_button_pressed() -> void:
+	_is_paused = false
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_stats_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
