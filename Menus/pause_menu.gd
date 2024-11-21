1ed907bcd6e42a2d6267a0ede04c7454111bcb70
MrHammer117 <Starkey2893@gmail.com>
2024-11-20 22:07:14 -0500

    cleanup

--- Menus/pause_menu.gd
+++ Menus/pause_menu.gd
@@ -1,18 +1,14 @@
 extends Control
 
 var _is_paused:bool = false:
-	set = set_paused
+	set(value):
+		_is_paused = value
+		get_tree().paused = _is_paused
+		visible = _is_paused
 
 func _unhandled_input(event:InputEvent) -> void:
 	if event.is_action_pressed("escape"):
 		_is_paused = !_is_paused
-
-
-func set_paused(value:bool) -> void:
-	_is_paused = value
-	get_tree().paused = _is_paused
-	visible = _is_paused
-	
 
 func _on_resume_button_pressed() -> void:
 	_is_paused = false
