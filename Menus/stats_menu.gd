b9380fdab040708dc62e869c85e720f741a2f2f2
MrHammer117 <Starkey2893@gmail.com>
2024-12-02 20:50:35 -0500

    fix

---
+++ Menus/stats_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+#$Label.text = "Fuel = %d" % fuel - this is how i should access the labels to display variables
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
